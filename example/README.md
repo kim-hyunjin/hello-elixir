84265797aa593071fb5c7636823c63fcf16d2822
kim-hyunjin <hyunjin1612@gmail.com>
2024-04-10 17:58:43 +0900

    escript

--- example/README.md
+++ example/README.md
@@ -19,3 +19,7 @@ Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_do
 and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
 be found at <https://hexdocs.pm/example>.
 
+# 빌드하기
+```
+$ mix escript.build
+```
--- example/lib/example.ex
+++ example/lib/example.ex
@@ -16,3 +16,24 @@ defmodule Example do
     :world
   end
 end
+
+defmodule ExampleApp.CLI do
+  def main(args \\ []) do
+    args
+    |> parse_args()
+    |> response()
+    |> IO.puts()
+  end
+
+  defp parse_args(args) do
+    {opts, word, _} =
+      args
+      |> OptionParser.parse(switches: [upcase: :boolean])
+
+    {opts, List.to_string(word)}
+  end
+
+  defp response({opts, word}) do
+    if opts[:upcase], do: String.upcase(word), else: word
+  end
+end
